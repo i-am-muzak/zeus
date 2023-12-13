ba8200f26b6e9f3ff694b50b081f208b09694aa0
Hasan Muzak <hasan@apps.com.tr>
2023-12-13 23:22:12 +0300

    Update: Infinite grid

--- frontend/src/components/CanvasEditor/Editor.vue
+++ frontend/src/components/CanvasEditor/Editor.vue
@@ -1,27 +1,18 @@
 <template>
   <div>
     <div>
-      <canvas
-        ref="maskBackgroundStage"
-      ></canvas>
-      <canvas
-        @mousedown="baseEditorMouseDown"
-        @mouseup="baseEditorMouseUp"
-        @mousemove="baseEditorMouseMove"
-        ref="baseEditor"
-      ></canvas>
+      <canvas @mousedown="baseEditorMouseDown" @mouseup="baseEditorMouseUp" @mousemove="baseEditorMouseMove"
+        ref="baseEditor"></canvas>
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
 import { ref, type Ref, computed, onMounted } from "vue";
 
 const baseEditor: Ref<HTMLCanvasElement | null> = ref(null);
-const maskBackgroundStage: Ref<HTMLCanvasElement | null> = ref(null);
-  
+
 const context: Ref<CanvasRenderingContext2D | null> = ref(null);
-const gridOffset = ref(50);
+const gridSize = ref(50);
 const imagePosition: Ref<any> = ref({
   x: 0,
   y: 0,
@@ -34,126 +25,114 @@ const lastMaskPosition: Ref<any> = ref({
   y: 0,
 });
 
-const computedGridCircles = computed(() => {
-  var coords = [];
-  for (
-    let x = gridOffset.value / 2;
-    x < window.innerWidth;
-    x += gridOffset.value
-  ) {
-    for (
-      let y = gridOffset.value / 2;
-      y < window.innerHeight;
-      y += gridOffset.value
-    ) {
-      coords.push({
-        x,
-        y,
-      });
-    }
-  }
-  console.log(coords);
-
-  return coords;
-});
+const isDragging = ref(false);
+const startDragPositions = ref({ x: 0, y: 0 });
+const currentOffsetPositions = ref({ x: 0, y: 0 });
 
 function drawGridCircles() {
-  console.log(computedGridCircles.value.length);
-  if (baseEditor.value) {
-    const ctx = baseEditor.value.getContext("2d");
+  if (context.value) {
+    context.value.beginPath();
 
-    if (ctx) {
-      for (let i = 0; i < computedGridCircles.value.length; i++) {
-        const coord = computedGridCircles.value[i];
-        ctx.rect(coord.x, coord.y, 3, 3);
-        ctx.fillStyle = "#333";
-        ctx.fill();
+    const startX = Math.floor(-currentOffsetPositions.value.x / gridSize.value) * gridSize.value + gridSize.value / 2;
+    const startY = Math.floor(-currentOffsetPositions.value.y / gridSize.value) * gridSize.value + gridSize.value / 2;
+
+    const finishX = startX + window.innerWidth + gridSize.value * 5
+    const finishY = startY + window.innerHeight + gridSize.value * 5
+
+    for (let x = startX; x < finishX; x += gridSize.value) {
+      for (let y = startY; y < finishY; y += gridSize.value) {
+        context.value.rect(x + currentOffsetPositions.value.x, y + currentOffsetPositions.value.y, 3, 3)
       }
     }
+
+
+    context.value.fillStyle = "#333";
+    context.value.fill();
   }
-}
-
-function drawImage(canvasWidth: number, canvasHeight: number) {
-  if (context.value && canvasWidth && canvasHeight) {
-    const context = baseEditor.value?.getContext("2d");
-    if (context) {
-      const startX = (canvasWidth - canvasBaseImage.value.width) / 2;
-      const startY = (canvasHeight - canvasBaseImage.value.height) / 2;
-
-      context.drawImage(
-        canvasBaseImage.value,
-        startX,
-        startY,
-        canvasBaseImage.value.width,
-        canvasBaseImage.value.height
-      );
-
-      imagePosition.value.x = startX;
-      imagePosition.value.y = startY;
-    }
-  }
-}
-
-function isMouseOnImage(clientX: number, clientY: number) {
-  if (canvasBaseImage.value) {
-    if (
-      clientX >= imagePosition.value.x &&
-      clientX <= imagePosition.value.x + canvasBaseImage.value.width &&
-      clientY >= imagePosition.value.y &&
-      clientY <= imagePosition.value.y + canvasBaseImage.value.height
-    ) {
-      return true;
-    }
-  }
-  return false;
 }
 
 function baseEditorMouseMove(event: MouseEvent) {
-  const isCursorOnImage = isMouseOnImage(event.clientX, event.clientY);
-  if (isCursorOnImage && isMasking.value) {
-    // Here we can draw masks.
-    if (context.value) {
-      
-      context.value.globalCompositeOperation = "destination-out";
+  // const isCursorOnImage = isMouseOnImage(event.clientX, event.clientY);
+  // if (isCursorOnImage && isMasking.value) {
+  //   // Here we can draw masks.
+  //   if (context.value) {
+  //     context.value.globalCompositeOperation = "destination-out";
 
-      context.value.lineTo(event.clientX, event.clientY);
-      context.value.lineWidth = 30;
-      context.value.lineCap = "round";
-      context.value.lineJoin = "round";
-      context.value.stroke();
-    }
+  //     context.value.lineTo(event.clientX, event.clientY);
+  //     context.value.lineWidth = 30;
+  //     context.value.lineCap = "round";
+  //     context.value.lineJoin = "round";
+  //     context.value.stroke();
+  //   }
+  // }
+  if (isDragging.value) {
+    const deltaX = event.clientX - startDragPositions.value.x;
+    const deltaY = event.clientY - startDragPositions.value.y;
+
+    currentOffsetPositions.value.x += deltaX;
+    currentOffsetPositions.value.y += deltaY;
+
+    updateStartDragPositions(event.clientX, event.clientY);
+    updatePanning();
   }
 }
 
+function updateStartDragPositions(x: number, y: number) {
+  startDragPositions.value = {
+    x: x,
+    y: y,
+  };
+}
+
 function baseEditorMouseDown(event: MouseEvent) {
-  const isCursorOnImage = isMouseOnImage(event.clientX, event.clientY);
-  if (isCursorOnImage) {
-    isMasking.value = true;
+  // const isCursorOnImage = isMouseOnImage(event.clientX, event.clientY);
+  // if (isCursorOnImage) {
+  //   isMasking.value = true;
 
-    if (context.value) {
-      if (!lastMaskPosition.value.x) {
-        lastMaskPosition.value.x = event.clientX;
-      }
-      if (!lastMaskPosition.value.y) {
-        lastMaskPosition.value.y = event.clientY;
-      }
+  //   if (context.value) {
+  //     if (!lastMaskPosition.value.x) {
+  //       lastMaskPosition.value.x = event.clientX;
+  //     }
+  //     if (!lastMaskPosition.value.y) {
+  //       lastMaskPosition.value.y = event.clientY;
+  //     }
 
-      context.value.beginPath();
-      context.value.globalCompositeOperation = "destination-out";
-      context.value.lineTo(event.clientX, event.clientY);
-      context.value.lineWidth = 30;
-      context.value.lineCap = "round";
-      context.value.lineJoin = "round";
-      context.value.stroke();
+  //     context.value.beginPath();
+  //     context.value.globalCompositeOperation = "destination-out";
+  //     context.value.lineTo(event.clientX, event.clientY);
+  //     context.value.lineWidth = 30;
+  //     context.value.lineCap = "round";
+  //     context.value.lineJoin = "round";
+  //     context.value.stroke();
 
-      lastMaskPosition.value.x = event.clientX;
-      lastMaskPosition.value.y = event.clientY;
-    }
-  }
+  //     lastMaskPosition.value.x = event.clientX;
+  //     lastMaskPosition.value.y = event.clientY;
+  //   }
+  // }
+
+  isDragging.value = true;
+  updateStartDragPositions(event.clientX, event.clientY);
 }
 
 function baseEditorMouseUp(event: MouseEvent) {
   isMasking.value = false;
+  isDragging.value = false;
+}
+
+function clearCanvas() {
+  if (context.value) {
+    context.value.clearRect(
+      -3 * window.innerWidth,
+      -3 * window.innerHeight,
+      6 * window.innerWidth,
+      6 * window.innerHeight
+    );
+  }
+}
+
+function updatePanning() {
+  clearCanvas();
+  drawGridCircles();
 }
 
 onMounted(() => {
@@ -165,7 +144,8 @@ onMounted(() => {
 
     canvasBaseImage.value.src = "https://i.hizliresim.com/7hyg5rc.jpg";
     canvasBaseImage.value.onload = () => {
-      drawImage(baseEditor.value?.width || 0, baseEditor.value?.height || 0);
+      // drawImage(baseEditor.value?.width || 0, baseEditor.value?.height || 0);
+      console.log("img load");
     };
   }
 
