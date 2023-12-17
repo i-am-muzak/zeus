5f6172d92314cc95e64bcd6e7e9b5f882608308e
Hasan Muzak <hasan@apps.com.tr>
2023-12-17 10:52:06 +0300

    Inf. canvas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, type Ref } from "vue";
+import {ref, type Ref} from "vue";
 
 import KonvaEditor from "@/components/KonvaEditor/Editor.vue";
 import InfiniteCanvasEditor from "@/components/InfiniteCanvas/InfiniteCanvasEditor.vue";
@@ -9,48 +9,48 @@ import TopCanvasMenu from "@/components/Menus/TopCanvasMenu.vue";
 import LayerCanvasMenu from "@/components/Menus/LayerCanvasMenu.vue";
 import ForgeCanvasMenu from "@/components/Menus/ForgeCanvasMenu.vue";
 
-import { NConfigProvider, darkTheme } from "naive-ui";
+import {NConfigProvider, darkTheme} from "naive-ui";
 
 const selectedTool: Ref<string | null> = ref(null);
 
 function handleOnToolChange(tool: string) {
-  selectedTool.value = tool;
+    selectedTool.value = tool;
 }
 
 </script>
 
 <template>
-  <n-config-provider :theme="darkTheme">
-    <div>
-      <div>
-        <!-- <TopCanvasMenu @change="handleOnToolChange" />
-        <LayerCanvasMenu />
-        <ForgeCanvasMenu /> -->
-      </div>
-      <!-- <KonvaEditor :tool="selectedTool" /> -->
-      <!-- <Editor /> -->
-      <InfiniteCanvasEditor />
-    </div>
-  </n-config-provider>
+    <n-config-provider :theme="darkTheme">
+        <div>
+            <div>
+                <!-- <TopCanvasMenu @change="handleOnToolChange" />
+                <LayerCanvasMenu />
+                <ForgeCanvasMenu /> -->
+            </div>
+            <!-- <KonvaEditor :tool="selectedTool" /> -->
+            <Editor/>
+            <!---<InfiniteCanvasEditor /> -->
+        </div>
+    </n-config-provider>
 </template>
 
 <style scoped lang="less">
 .layerMenu {
-  z-index: 10;
-  right: 30px;
-  top: 50%;
-  transform: translate(0, -50%);
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
-  min-width: 235px;
+    z-index: 10;
+    right: 30px;
+    top: 50%;
+    transform: translate(0, -50%);
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+    min-width: 235px;
 }
 
 .leftMenu {
-  z-index: 10;
-  left: 0;
-  top: 0;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
-  width: 270px;
-  height: 100%;
-  max-height: 100%;
+    z-index: 10;
+    left: 0;
+    top: 0;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+    width: 270px;
+    height: 100%;
+    max-height: 100%;
 }
 </style>
--- frontend/src/components/InfiniteCanvas/InfiniteCanvasEditor.vue
+++ frontend/src/components/InfiniteCanvas/InfiniteCanvasEditor.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <canvas ref="canvasEditor"></canvas>
-  </div>
+    <div>
+        <canvas ref="canvasEditor"></canvas>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref, type Ref, onMounted } from "vue";
+import {ref, type Ref, onMounted, computed} from "vue";
 import Canvas, {
-  type InfiniteCanvasRenderingContext2D,
-  type InfiniteCanvas,
+    type InfiniteCanvasRenderingContext2D,
+    type InfiniteCanvas,
+    type TransformationEvent
 } from "ef-infinite-canvas";
 
 const infiniteCanvas: Ref<InfiniteCanvas | null> = ref(null);
@@ -16,36 +17,70 @@ const canvasEditor: Ref<HTMLCanvasElement | null> = ref(null);
 const context: Ref<InfiniteCanvasRenderingContext2D | null> = ref(null);
 const canvasBaseImage: Ref<HTMLImageElement | null> = ref(new Image());
 
+const circleGridOffset = ref(100);
+
+// Scale Data
+const currentScale = ref(1);
+
 function drawImage() {
-  if (context.value && canvasBaseImage.value) {
-    context.value.drawImage(
-      canvasBaseImage.value,
-      (context.value.canvas.width - canvasBaseImage.value.width) / 2,
-      (context.value.canvas.height - canvasBaseImage.value.height) / 2,
-    );
-  }
+    if (context.value && canvasBaseImage.value) {
+        context.value.beginPath()
+        context.value.drawImage(
+            canvasBaseImage.value,
+            (context.value.canvas.width - canvasBaseImage.value.width) / 2,
+            (context.value.canvas.height - canvasBaseImage.value.height) / 2,
+        );
+    }
+}
+
+function drawGridCircles() {
+    if (context.value) {
+        context.value.beginPath()
+
+        for (let x = 0; x < window.innerWidth; x += circleGridOffset.value) {
+            for (let y = 0; y < window.innerHeight; y += circleGridOffset.value) {
+                context.value.rect(x, y, 20, 20)
+            }
+        }
+        context.value.fillStyle = "#fff"
+        context.value.fill()
+
+    }
 }
 
 onMounted(() => {
-  if (canvasEditor.value) {
-    // context.value = canvasEditor.value.getContext("2d");
-    canvasEditor.value.width = window.innerWidth;
-    canvasEditor.value.height = window.innerHeight;
+    if (canvasEditor.value) {
+        // context.value = canvasEditor.value.getContext("2d");
+        canvasEditor.value.width = window.innerWidth;
+        canvasEditor.value.height = window.innerHeight;
 
-    setTimeout(() => {
-      infiniteCanvas.value = new Canvas(canvasEditor.value);
-      if (infiniteCanvas.value) {
-        context.value = infiniteCanvas.value.getContext("2d");
 
-        // Set Image properties.
-        if (canvasBaseImage.value) {
-          canvasBaseImage.value.src = "https://i.hizliresim.com/7hyg5rc.jpg";
-          canvasBaseImage.value.onload = () => {
-            drawImage();
-          };
-        }
-      }
-    }, 1000);
-  }
+        setTimeout(() => {
+            infiniteCanvas.value = new Canvas(canvasEditor.value);
+            if (infiniteCanvas.value) {
+                context.value = infiniteCanvas.value.getContext("2d");
+
+                // Set Image properties.
+                if (canvasBaseImage.value) {
+                    canvasBaseImage.value.src = "https://i.hizliresim.com/7hyg5rc.jpg";
+                    canvasBaseImage.value.onload = () => {
+                        drawGridCircles()
+                    };
+                }
+            }
+
+            // Infinite Canvas Event Handlers
+            infiniteCanvas.value.ondraw = (event: TransformationEvent) => {
+                currentScale.value = 1 / event.transformation.a;
+            }
+
+            infiniteCanvas.value.onwheel = (event) => {
+                if (currentScale.value >= 0.5 && currentScale.value <= 2) {
+                    event.preventDefault()
+                }
+            }
+
+        }, 1000);
+    }
 });
 </script>
